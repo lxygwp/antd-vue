943fc561de779b0cbb54be33254a5bd34f40356e
lxygwp <1203381320@qq.com>
2019-05-10 17:39:19 +0800

    按需引入button icon

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <a-button type="primary">Button></a-button> -->
+    <a-button type="primary" icon="search">Button</a-button>
+    <a-button type="primary" icon="download">Button</a-button>
+    <div class="icons-list">
+      <a-icon type="home" />
+      <a-icon type="setting" theme="filled" />
+      <a-icon type="smile" theme="outlined" />
+      <a-icon type="sync" spin />
+      <a-icon type="loading" />
+    </div>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -25,5 +33,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+ .anticon {
+  margin-right: 6px;
+  font-size: 24px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-// import Antd from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
-Vue.config.productionTip = false
-// Vue.use(Antd)
+import Vue from "vue";
+import Button from "ant-design-vue/lib/button";
+import Icon from 'ant-design-vue/lib/icon'
+import "ant-design-vue/dist/antd.css";
+import App from "./App";
+
+
+Vue.config.productionTip = false;
+Vue.use(Icon)
+Vue.use(Button)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
